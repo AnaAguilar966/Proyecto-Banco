2324aadb5222cf80145a19b51a1b66fea4a7e0da
arumando <armandobandera8@gmail.com>
2026-06-28 17:02:08 -0600

    s
    s

--- banco/src/entidades/Abstracta.java
+++ banco/src/entidades/Abstracta.java
@@ -2,7 +2,7 @@ package entidades;
 
 public abstract class Abstracta {
 
-    // Solo el método abstracto que se usará (sobreescribirá) en las demás clases
+
     public abstract String obtenerDetalles();
 
 }
